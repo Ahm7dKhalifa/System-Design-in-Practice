603d122f6c54b8f3e94ab1b34a7d4d476cae6c48
ahmed khalifa <ahm7dkhalifa@gmail.com>
2022-11-12 15:11:41 +0200

    Update Clean Architecture\E-Commerce Folder

---
+++ Clean Architecture/E-Commerce/Web.Company.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web.Company.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
